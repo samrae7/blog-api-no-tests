1f1a1026ae3796a2650f1c82340baec3999ef554
Sam Rae <samuelrae@gmail.com>
2018-09-06 07:44:43 +0100

    added migration for 'intro' on Post

--- Migrations/20180906064157_intro.Designer.cs
+++ Migrations/20180906064157_intro.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BlogApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogApi.Migrations
 {
     [DbContext(typeof(PostContext))]
-    partial class PostContextModelSnapshot : ModelSnapshot
+    [Migration("20180906064157_intro")]
+    partial class intro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
