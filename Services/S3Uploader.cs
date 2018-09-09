b076eb7b9d8ce2308674ade4c58fd1ad27bc2c89
Sam Rae <samuelrae@gmail.com>
2018-09-09 08:09:33 +0100

    refactor

--- Services/S3Uploader.cs
+++ Services/S3Uploader.cs
@@ -14,32 +14,41 @@ namespace BlogApi.Services
   public class AmazonUploader
   {
     private IAmazonS3 client;
-    private const string bucketName = "secret-london-blog"; 
+    private const string bucketName = "secret-london-blog";
     private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUWest2;
 
-    public AmazonUploader(string AWS_KEY, string AWS_SECRET) {
+    public AmazonUploader(string AWS_KEY, string AWS_SECRET)
+    {
       client = new AmazonS3Client(AWS_KEY, AWS_SECRET, bucketRegion);
     }
 
     public async Task<ListObjectsResponse> ListingObjectsAsync()
-    {            
-        ListObjectsRequest request = new ListObjectsRequest
-        {
-            BucketName = bucketName
-        };
-        return await client.ListObjectsAsync(request);
-    }
-    public async Task<PutObjectResponse> sendMyFileToS3(System.IO.Stream inputStream, string contentType, long contentLength, string key)
     {
-      PutObjectRequest request = new PutObjectRequest
+      ListObjectsRequest request = new ListObjectsRequest
+      {
+        BucketName = bucketName
+      };
+      return await client.ListObjectsAsync(request);
+    }
+    public async Task<string> sendMyFileToS3(System.IO.Stream inputStream, string contentType, long contentLength, string key)
+    {
+      var request = new PutObjectRequest
       {
         BucketName = bucketName,
         Key = key,
         ContentType = contentType,
-        InputStream = inputStream 
+        InputStream = inputStream
       };
       request.Headers.ContentLength = contentLength;
-      return await client.PutObjectAsync(request);
+      PutObjectResponse awsResponse = await client.PutObjectAsync(request);
+      if (awsResponse.HttpStatusCode == HttpStatusCode.OK)
+      {
+        return key;
+      }
+      else
+      {
+        return "error";
+      }
     }
   }
 }
