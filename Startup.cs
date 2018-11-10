312df027407472dbe180fb88c01aaa4d2ea9f9c1
Sam Rae <samuelrae@gmail.com>
2018-11-10 15:35:37 +0000

    add explantory comments

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace BlogApi
       Configuration = builder.Build();
     }
     
+    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc()
@@ -47,6 +48,8 @@ namespace BlogApi
           c.SwaggerDoc("v1", new Info { Title = "Sams Blog API", Version = "v1" });
       });
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app)
     {
       app.UseCors(builder =>
