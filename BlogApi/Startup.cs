ae7127e008dd46668e4b66558e07444c05b0d5de
Sam Rae <samuelrae@gmail.com>
2018-12-01 22:59:27 +0000

    configure auth

--- BlogApi/Startup.cs
+++ BlogApi/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BlogApi.Models;
+using BlogApi.Auth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BlogApi
 {
@@ -29,6 +32,26 @@ namespace BlogApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      string domain = $"https://{Configuration["Auth0:Domain"]}/";
+      services.AddAuthentication(options =>
+      {
+          options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+          options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+      }).AddJwtBearer(options =>
+      {
+          options.Authority = domain;
+          options.Audience = Configuration["Auth0:ApiIdentifier"];
+      });
+
+      services.AddAuthorization(options =>
+      {
+          options.AddPolicy("create:posts", policy => policy.Requirements.Add(new HasScopeRequirement("read:messages", domain)));
+      });
+
+      // register the scope authorization handler
+      services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
       services.AddMvc()
               .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -69,6 +92,9 @@ namespace BlogApi
       {
           c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sams Blog API V1");
       });
+
+      app.UseAuthentication();
+
       app.UseMvc();
     }
   }
